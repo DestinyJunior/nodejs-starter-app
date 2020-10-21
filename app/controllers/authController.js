e9a0b26f64c4c8b64c95290901f8d5b0de0a1dda
Sunday Destiny <destinyjuniorsdj@gmail.com>
2020-10-21 17:03:58 +0100

    added more fields to user model

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -9,13 +9,14 @@ const User = require('../models/User');
 // @access    Public
 // eslint-disable-next-line no-unused-vars
 exports.register = asyncHandler(async (req, res, next) => {
-  const { name, email, password, role } = req.body;
+  const { name, email, password, phone, role } = req.body;
 
   // register user
   const user = await User.create({
     name,
     email,
     password,
+    phone,
     role
   });
 
